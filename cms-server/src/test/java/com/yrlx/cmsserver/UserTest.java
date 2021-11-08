3fb4ae5f734dd232b193147b97aef7401880d558
xuping119 <xuping119@sohu.com>
2021-11-08 15:47:41 +0800

    增加mybatis+mysql

---
+++ cms-server/src/test/java/com/yrlx/cmsserver/UserTest.java
@@ -0,0 +1,39 @@
+package com.yrlx.cmsserver;
+
+import java.io.IOException;
+import java.io.Reader;
+import com.yrlx.cmsserver.entity.User;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+
+    @Test
+    public void checkUsertest(){
+        //定义读取文件名
+        String resources = "config/mybatis-config.xml";
+        //创建流
+        Reader reader=null;
+        try {
+            //读取mybatis-config.xml文件到reader对象中
+            reader= Resources.getResourceAsReader(resources);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        //初始化mybatis,创建SqlSessionFactory类的实例
+        SqlSessionFactory sqlMapper=new SqlSessionFactoryBuilder().build(reader);
+        //创建session实例
+        SqlSession session=sqlMapper.openSession();
+        //传入参数查询，返回结果
+        User user=session.selectOne("checkUser","xp");
+        //输出结果
+        System.out.println(user.toString());
+        //关闭session
+        session.close();
+
+    }
+}
