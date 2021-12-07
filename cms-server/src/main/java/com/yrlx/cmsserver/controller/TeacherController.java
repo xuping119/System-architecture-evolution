c53730426948b2c860f75f54ae62c4b102ccc4dd
xuping119 <xuping119@sohu.com>
2021-12-07 16:47:05 +0800

    支持json参数解析，非springboot自动转换为bean对象

--- cms-server/src/main/java/com/yrlx/cmsserver/controller/TeacherController.java
+++ cms-server/src/main/java/com/yrlx/cmsserver/controller/TeacherController.java
@@ -1,11 +1,10 @@
 package com.yrlx.cmsserver.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.yrlx.cmsserver.common.JsonResult;
 import com.yrlx.cmsserver.service.TeacherService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -17,6 +16,14 @@ public class TeacherController {
 
     @RequestMapping("/getteacher")
     public JsonResult getTeacher(@RequestParam(value = "id")  int id){
+        return teacherService.getTeachers(id);
+    }
+
+    @RequestMapping("/getteacher2")
+    public JsonResult getTeacher2(@RequestBody String jsonParams){
+
+        JSONObject jsonObject = JSON.parseObject(jsonParams);
+        int id  = jsonObject.getIntValue("id");
         return teacherService.getTeachers(id);
     }
 }
